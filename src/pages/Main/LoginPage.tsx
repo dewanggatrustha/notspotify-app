5622c4f6334ec8abd04f3634c8de10da71b9e94b
dewanggatrustha <57487611+dewanggatrustha@users.noreply.github.com>
2022-04-22 22:15:12 +0700

    Redesign Login page

--- src/pages/Main/LoginPage.tsx
+++ src/pages/Main/LoginPage.tsx
@@ -3,7 +3,7 @@ import { useDispatch } from "react-redux";
 import { toast } from "react-toastify";
 import { getUserProfile, getSpotifyAuth } from "../../lib/apiSpotify";
 import { login } from "../../Redux/authSlice";
-import { Flex, Button } from "@chakra-ui/react";
+import { Flex, Button, Heading, Text, VStack, HStack } from "@chakra-ui/react";
 
 const LoginPage = () => {
 	const dispatch = useDispatch();
@@ -38,20 +38,36 @@ const LoginPage = () => {
 		<Flex
 			h="100vh"
 			alignItems="center"
+			flexDirection="column"
 			justifyContent="center"
 			bgGradient="linear(purple.900 0%, gray.900 20%)"
 		>
-			<Button
-				as="a"
-				bgColor="green.500"
-				color="white"
-				_focus={{ boxShadow: "none" }}
-				_hover={{ bgColor: "green.400" }}
-				borderRadius="25"
-				href={getSpotifyAuth()}
-			>
-				Log In with Spotify
-			</Button>
+			<VStack spacing={8} textAlign="center">
+				<Heading as="h1" size="4xl" color="green.400">
+					<Text as="span" color="white">
+						Welcome to
+					</Text>{" "}
+					NotSpotify.
+				</Heading>
+				<Text color="white" w="2xl" fontSize="xl">
+					Lets log in to your account to enjoy the Spotify integrated app
+					experience where you can create playlist with your favorite
+					songs..
+				</Text>
+				<HStack>
+					<Button
+						as="a"
+						bgColor="green.500"
+						color="white"
+						_focus={{ boxShadow: "none" }}
+						_hover={{ bgColor: "green.400" }}
+						borderRadius="25"
+						href={getSpotifyAuth()}
+					>
+						Get Started
+					</Button>
+				</HStack>
+			</VStack>
 		</Flex>
 	);
 };
